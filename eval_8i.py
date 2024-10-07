34ad82b664ae66e51d8523561d99340ac2a98104
David Dong <hidaviddong@gmail.com>
2024-10-07 09:22:27 +0000

    fix: move tensor to cpu in eval_8i

--- eval_8i.py
+++ eval_8i.py
@@ -5,18 +5,17 @@ import torch
 import MinkowskiEngine as ME
 from model.Network import MyNet
 
-
 def parse_args():
   parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   # For 8x upsampling
-  parser.add_argument("--pretrained", default='./ckpts/8x_0x_ks5/iter64000.pth', help='Path to pretrained model')
+  parser.add_argument("--pretrained", default='./ckpts/8x_0x_ks7/iter64000.pth', help='Path to pretrained model')
   # For 4x upsampling
   # parser.add_argument("--pretrained", default='./ckpts/4x_0x_ks5/iter64000.pth', help='Path to pretrained model')
-  parser.add_argument("--prefix", type=str, default='8x_0x_ks5', help="prefix of checkpoints/logger, etc.")
+  parser.add_argument("--prefix", type=str, default='8x_0x_ks7', help="prefix of checkpoints/logger, etc.")
   parser.add_argument("--up_ratio", default=8, help='Upsample Ratio')
   
-  parser.add_argument("--test_dataset", default='../Data/8i/8x/')
+  parser.add_argument("--test_dataset", default='../Data/8i_test/8x/longdress/Ply/')
   parser.add_argument("--save_loc", default='results_8i/')
   parser.add_argument("--last_kernel_size", type=int, default=5, help='The final layer kernel size, coordinates get expanded by this')
   
@@ -56,8 +55,7 @@ def kdtree_partition(pc, max_num):
 
 args = parse_args()
 test_files = glob.glob(args.test_dataset+'*.ply')
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = MyNet(last_kernel_size=args.last_kernel_size).to(device)
 ckpt = torch.load(args.pretrained)
 model.load_state_dict(ckpt['model'])
@@ -82,8 +80,8 @@ for i, pc in enumerate(test_files):
         part_T = np.random.randint(0, part.max(), size=(part.shape[0]*args.up_ratio, 3))
         p2 = ME.utils.batched_coordinates([part_T])
         f = torch.from_numpy(np.vstack(np.expand_dims(np.ones(p.shape[0]), 1))).float()
-        x = ME.SparseTensor(feats=f, coords=p).to(device)
-        
+        x = ME.SparseTensor(features=f.to(device), coordinates=p.to(device))
+   
         with torch.no_grad():
             out, _, _, _ = model(x, coords_T=p2, device=device, prune=True)
             
@@ -91,8 +89,9 @@ for i, pc in enumerate(test_files):
       
     rec_pc = torch.cat(out_list, 0)
     print("Number of points in input point cloud : ", coords.shape[0])
-    print("Number of points in output rec point cloud : ", rec_pc.numpy().shape[0])
     rec_pcd = o3d.geometry.PointCloud()
-    rec_pcd.points = o3d.utility.Vector3dVector(rec_pc)
+    rec_pc_cpu = rec_pc.cpu().numpy()
+    rec_pcd.points = o3d.utility.Vector3dVector(rec_pc_cpu)
+    print("Number of points in output rec point cloud : ", rec_pc_cpu.shape[0])
     recfile = os.path.join(save_path, file_name)
     o3d.io.write_point_cloud(recfile, rec_pcd, write_ascii=True)
